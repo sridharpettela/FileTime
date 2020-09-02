970b27acc67d697054a255fe70b6eef7433e8b1b
sridharpettela <44844967+sridharpettela@users.noreply.github.com>
2020-09-02 21:33:20 +0530

    Changes

---
+++ Tests/FileTime.EFM.Test/UserServiceWrapperTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileTime.EFM.Test
+{
+	[TestClass]
+	public class UserServiceWrapperTest
+	{
+		[TestMethod]
+		public void Login()
+		{
+		}
+	}
+}
